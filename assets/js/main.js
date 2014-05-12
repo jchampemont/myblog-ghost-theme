c5f36bae2e13039d1b153bb1354f2e38112c9de2
Jean Champémont <jchampemont@ippon.fr>
2014-05-12 18:11:34 +0200

    Add hack to allow post cover image.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 $(function() {
   $('article img').addClass('img-responsive');
   $('code').addClass('prettyprint');
+  $(article).each(function() {
+    if($(this).find('img[alt="img-post-cover"]').length > 0) {
+      var imgPostCover = $($(this).find('img[alt="img-post-cover"]')[0]);
+      $(this).find('article-cover').attr('src', imgPostCover.attr('src'));
+      imgPostCover.remove();
+    } else {
+      $(this).find('article-cover').remove();
+    }
+  });
 });
